b68601d49c69d5189201c700b3d9291f7bf03060
Robert Brower <robert.brower@data2impact.com>
2016-03-13 13:39:38 +0100

    Added branch name and full name validation

--- src/app/components/component1/component1.component.ts
+++ src/app/components/component1/component1.component.ts
@@ -3,8 +3,17 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'component1',
   template: require('./component1.tpl.html'),
-  //styles: [ require('../../external_modules/gridforms/gridforms.css') ],
   host: {'flex': '', 'layout-padding': '', 'style': 'overflow: auto;'}
 })
 
-export class Component1Component {}
+export class Component1Component {
+    
+    submit(form: any) {
+        
+    }
+    
+    alert(s) {
+        console.log(s);
+    }
+    
+}
