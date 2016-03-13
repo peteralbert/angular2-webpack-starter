07079d02a647a8be8b6b7803464baa478a5d12ca
Robert Brower <robert.brower@data2impact.com>
2016-03-13 15:21:40 +0100

    Confirm nav away

--- src/app/components/component1/component1.component.ts
+++ src/app/components/component1/component1.component.ts
@@ -1,4 +1,7 @@
 import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/toPromise'
 
 @Component({
   selector: 'component1',
@@ -10,6 +13,10 @@ export class Component1Component {
     
     submit(form: any) {
         console.log(form);
+    }
+    
+    routerCanDeactivate() {
+        return Observable.of(window.confirm('If you leave this form your changes will be lost. Leave this form?')).toPromise();
     }
     
 }
